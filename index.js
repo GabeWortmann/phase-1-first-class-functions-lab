7917c2afdb3e0bd6f59727e24eea419e25f05fa8
Gabriel Wortmann <wortmanngabriel@gmail.com>
2023-10-03 21:24:28 -0500

    manipulates advanced higher order functions

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(arrayOfDrivers) {
+    return [arrayOfDrivers[0], arrayOfDrivers[1]]
+}
+
+function returnLastTwoDrivers(arrayOfDrivers) {
+    return [arrayOfDrivers[arrayOfDrivers.length-2], arrayOfDrivers[arrayOfDrivers.length-1]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(val) {
+    return function(val){
+        return val*5
+    }
+}
+
+function fareDoubler(val){
+    return val*2
+}
+
+function fareTripler(val){
+    return val*3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, function1){
+    return function1(arrayOfDrivers) 
+}
